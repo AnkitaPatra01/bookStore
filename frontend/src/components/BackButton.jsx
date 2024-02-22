497d85b9bb77cb36b93b589d5c4539a8318afb0e
Ankita Patra <ankitapatra@Ankitas-MacBook-Air.local>
2024-02-23 00:07:57 +0530

    back button added

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { BsArrowLeft } from 'react-icons/bs';
+
+const backButton = () => {
+  return (
+    <div className='flex' >
+      <Link to={Destination} className='bg-sky-800 text-white px-4 py-1 rounded-1g w-fit'>
+        <BsArrowLeft className='text-2xl'/>
+      </Link>
+    </div>
+  )
+}
+
+export default backButton
