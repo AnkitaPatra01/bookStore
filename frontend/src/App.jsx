f86d9e0c26e52d9409670702b404ce679ee9be7d
Ankita Patra <ankitapatra@Ankitas-MacBook-Air.local>
2024-02-22 23:55:12 +0530

    commited

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import {Routes, Router} from 'react-router-dom'
-import CreateBooks from './pages/CreateBooks';
-import DeleteBook from './pages/DeleteBook';
-import EditBook from './pages/EditBook';
-import Home from './pages/Home';
-import ShowBook from './pages/ShowBook';
+import {Routes, Route} from 'react-router-dom'
+import CreateBooks from './pages/CreateBooks.jsx';
+import DeleteBook from './pages/DeleteBook.jsx';
+import EditBook from './pages/EditBook.jsx';
+import Home from './pages/Home.jsx';
+import ShowBook from './pages/ShowBook.jsx';
 
 const App = () => {
   return(
     <Routes>
-      <Router path='/' element = {<Home/>} />
-      <Router path='/books/create' element = {<CreateBooks/>} />
-      <Router path='/books/details/:id' element = {<ShowBook/>} />
-      <Router path='/books/edit/:id' element = {<EditBook/>} />
-      <Router path='/books/delete/:id' element = {<DeleteBook/>} />
+      <Route path='/' element = {<Home/>} />
+      <Route path='/books/create' element = {<CreateBooks/>} />
+      <Route path='/books/details/:id' element = {<ShowBook/>} />
+      <Route path='/books/edit/:id' element = {<EditBook/>} />
+      <Route path='/books/delete/:id' element = {<DeleteBook/>} />
     </Routes>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js, ts, jsx, tsx}"
+    "./src/**/*.{js, jsx, ts, tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/Spinner.jsx',
+    //'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
